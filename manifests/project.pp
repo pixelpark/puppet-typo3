11bdf2e22e93c301c916fd44367bcf42a5ae6d32
Philipp Dallig <philipp.dallig@pixelpark.com>
2017-02-06 16:25:14 +0100

    Set replace to false

--- manifests/project.pp
+++ manifests/project.pp
@@ -134,13 +134,14 @@ define typo3::project (
     "${site_path}/typo3conf/l10n",
     "${site_path}/typo3conf/ext"
   ]:
-    ensure => 'directory',
-    mode   => $dir_permission
+    replace => false,
+    ensure  => 'directory',
+    mode    => $dir_permission
   }
 
   file { "${site_path}/typo3conf/extTables.php":
-    ensure  => 'present',
-    replace => 'no',
+    ensure  => file,
+    replace => false,
     content => template('typo3/extTables.php.erb'),
     require => File["${site_path}/typo3conf"]
   }
@@ -148,8 +149,8 @@ define typo3::project (
   if $enable_install_tool == true {
 
     file { "${site_path}/typo3conf/ENABLE_INSTALL_TOOL":
-      ensure  => 'present',
-      replace => 'no',
+      ensure  => file,
+      replace => false,
       mode    => $file_permission,
       content => '',
       require => File["${site_path}/typo3conf"],
